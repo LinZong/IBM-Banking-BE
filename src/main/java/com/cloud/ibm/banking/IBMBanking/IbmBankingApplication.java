f9b6bedaaa692c9d3da0f24fc602fb30b7f34992
Nemesiss.Lin <353388639@qq.com>
2019-05-22 17:21:15 +0800

    Spring init

---
+++ src/main/java/com/cloud/ibm/banking/IBMBanking/IbmBankingApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.ibm.banking.IBMBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmBankingApplication.class, args);
+	}
+}
